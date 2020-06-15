59c071d0f4f2535c5cba33ea12c5aab3806914aa
paipoi8387 <nishimura.atsushi@d.mbox.nagoya-u.ac.jp>
2020-06-15 18:54:46 +0900

    mallocのエラー修正

--- malloc.c
+++ malloc.c
@@ -7,17 +7,17 @@ int main(void)
 {
     int nmem;
     int i;
-    for(i = 1; i <= 100){
-    // 1024個(charのサイズが1Bだとすると1024B = 1KB)のメモリを確保
-    nmem = 1024;
-    char *p;
-    p = (char *)malloc(sizeof(char) * nmem);
-    if (p == NULL) {
-        printf("ERROR,malloc\n");
-        exit(EXIT_FAILURE);
+    for (i = 1; i <= 100; i++) {
+        // 1024個(charのサイズが1Bだとすると1024B = 1KB)のメモリを確保
+        nmem = 1024;
+        char *p;
+        p = (char *)malloc(sizeof(char) * nmem);
+        if (p == NULL) {
+            printf("ERROR,malloc\n");
+            exit(EXIT_FAILURE);
+        }
+        printf("現在%d回目のmallocです\n", i);
     }
-    printf("現在%d回目のmallocです\n", i);
-    }
-	
+
     return 0;
 }
