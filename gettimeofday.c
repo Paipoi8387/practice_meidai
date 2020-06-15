dbc6aee16a6c0b633149f607e3ed6f2244ab783a
paipoi8387 <nishimura.atsushi@d.mbox.nagoya-u.ac.jp>
2020-06-15 13:43:09 +0900

    gettimeofday関数のcファイル作成

---
+++ gettimeofday.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <sys/time.h>
+
+int main(int argc, char **argv) {
+  struct timeval tv;
+  gettimeofday(&tv, NULL);
+  printf("%ld %06lu\n", tv.tv_sec, tv.tv_usec);
+  gettimeofday(&tv, NULL);
+  printf("%ld %06lu\n", tv.tv_sec, tv.tv_usec);
+  return 0;
+}
