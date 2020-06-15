7e8ae81e124212c1fb235760b7c4ab50057055c8
paipoi8387 <nishimura.atsushi@d.mbox.nagoya-u.ac.jp>
2020-06-15 18:39:19 +0900

    malloc用のプログラム追加

---
+++ malloc.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+ 
+// main
+int main(void)
+{
+    int nmem;
+    int i;
+    for(i = 1; i <= 100){
+    // 1024個(charのサイズが1Bだとすると1024B = 1KB)のメモリを確保
+    nmem = 1024;
+    char *p;
+    p = (char *)malloc(sizeof(char) * nmem);
+    if (p == NULL) {
+        printf("ERROR,malloc\n");
+        exit(EXIT_FAILURE);
+    }
+    printf("現在%d回目のmallocです\n", i);
+    }
+	
+    return 0;
+}
