0a1dda5c9b77ae4b9e8251a6e12ebf85e7f679a4
paipoi8387 <nishimura.atsushi@d.mbox.nagoya-u.ac.jp>
2020-06-15 19:05:30 +0900

    mallocの回数を1000回に変更

--- malloc.c
+++ malloc.c
@@ -7,7 +7,7 @@ int main(void)
 {
     int nmem;
     int i;
-    for (i = 1; i <= 100; i++) {
+    for (i = 1; i <= 1000; i++) {
         // 1024個(charのサイズが1Bだとすると1024B = 1KB)のメモリを確保
         nmem = 1024;
         char *p;
