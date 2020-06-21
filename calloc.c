265a359f291cd0e6adf4df63441ae2debd0a6d6d
paipoi8387 <nishimura.atsushi@d.mbox.nagoya-u.ac.jp>
2020-06-21 13:18:02 +0900

    メモリを確保後、アクセスする(0で埋める)場合のcalloc関数を用意

---
+++ calloc.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+ 
+// main
+int main(void)
+{
+    int nmem;
+    int i;
+    for (i = 1; i <= 100000; i++) {
+        // 1024個(charのサイズが1Bだとすると1024B = 1KB)のメモリを確保して0で埋める
+        nmem = 1024;
+        char *p;
+        p = (char *)calloc(nmem,sizeof(char));
+        if (p == NULL) {
+            printf("ERROR,malloc\n");
+            exit(EXIT_FAILURE);
+        }
+    }
+
+    return 0;
+}
