916d2924fec98c29b349fa553c4b300351e1c54f
paipoi8387 <nishimura.atsushi@d.mbox.nagoya-u.ac.jp>
2020-06-21 10:38:56 +0900

    gettimeofdayの出力を無しにしました

--- gettimeofday.c
+++ gettimeofday.c
@@ -4,8 +4,8 @@
 int main(int argc, char **argv) {
   struct timeval tv;
   gettimeofday(&tv, NULL);
-  printf("%ld %06lu\n", tv.tv_sec, tv.tv_usec);
-  gettimeofday(&tv, NULL);
-  printf("%ld %06lu\n", tv.tv_sec, tv.tv_usec);
+  //printf("%ld %06lu\n", tv.tv_sec, tv.tv_usec);
+  //gettimeofday(&tv, NULL);
+  //printf("%ld %06lu\n", tv.tv_sec, tv.tv_usec);
   return 0;
 }
