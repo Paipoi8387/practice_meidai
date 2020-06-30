9ad3e5f6e1999279f885668197fb574b260803af
paipoi8387 <nishimura.atsushi@d.mbox.nagoya-u.ac.jp>
2020-06-30 23:42:34 +0900

    gettimeofdayの回数を50回に変更

--- gettimeofday.c
+++ gettimeofday.c
@@ -3,9 +3,57 @@
 
 int main(int argc, char **argv) {
   struct timeval tv;
+  
   gettimeofday(&tv, NULL);
-  //printf("%ld %06lu\n", tv.tv_sec, tv.tv_usec);
-  //gettimeofday(&tv, NULL);
-  //printf("%ld %06lu\n", tv.tv_sec, tv.tv_usec);
+  gettimeofday(&tv, NULL);
+  gettimeofday(&tv, NULL);
+  gettimeofday(&tv, NULL);
+  gettimeofday(&tv, NULL);
+  gettimeofday(&tv, NULL);
+  gettimeofday(&tv, NULL);
+  gettimeofday(&tv, NULL);
+  gettimeofday(&tv, NULL);
+  gettimeofday(&tv, NULL);
+  gettimeofday(&tv, NULL);
+  gettimeofday(&tv, NULL);
+  gettimeofday(&tv, NULL);
+  gettimeofday(&tv, NULL);
+  gettimeofday(&tv, NULL);
+  gettimeofday(&tv, NULL);
+  gettimeofday(&tv, NULL);
+  gettimeofday(&tv, NULL);
+  gettimeofday(&tv, NULL);
+  gettimeofday(&tv, NULL);
+  gettimeofday(&tv, NULL);
+  gettimeofday(&tv, NULL);
+  gettimeofday(&tv, NULL);
+  gettimeofday(&tv, NULL);
+  gettimeofday(&tv, NULL);
+  gettimeofday(&tv, NULL);
+  gettimeofday(&tv, NULL);
+  gettimeofday(&tv, NULL);
+  gettimeofday(&tv, NULL);
+  gettimeofday(&tv, NULL);
+  gettimeofday(&tv, NULL);
+  gettimeofday(&tv, NULL);
+  gettimeofday(&tv, NULL);
+  gettimeofday(&tv, NULL);
+  gettimeofday(&tv, NULL);
+  gettimeofday(&tv, NULL);
+  gettimeofday(&tv, NULL);
+  gettimeofday(&tv, NULL);
+  gettimeofday(&tv, NULL);
+  gettimeofday(&tv, NULL);
+  gettimeofday(&tv, NULL);
+  gettimeofday(&tv, NULL);
+  gettimeofday(&tv, NULL);
+  gettimeofday(&tv, NULL);
+  gettimeofday(&tv, NULL);
+  gettimeofday(&tv, NULL);
+  gettimeofday(&tv, NULL);
+  gettimeofday(&tv, NULL);
+  gettimeofday(&tv, NULL);
+  gettimeofday(&tv, NULL);
+  
   return 0;
 }
